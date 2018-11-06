076af3cedae8d4d0ec281d528fb9e0b662d67578
wzj7531 <309910833@qq.com>
2018-11-06 15:04:42 +0800

    first commit

---
+++ src/SimpleWebApp/.wercker/projects/SimpleWebApp-a7283dc2-42f8-4e58-a098-7a0f6b56dbd8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+
+namespace SimpleWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .UseConfiguration(config)
+            .Build();
+
+            host.Run();
+        }
+    }
+}
